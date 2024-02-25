ca701aaf778867891f45d770174f1ba380c5daca
JUAN MANUEL URIBE <88215048+ojmuribe@users.noreply.github.com>
2024-02-25 13:17:40 +0100

    Add files via upload

---
+++ RfidKey/RfidKey.cpp
@@ -0,0 +1,110 @@
+#include "RfidKey.h"
+
+rfidKey::rfidKey(uint16_t cs, uint16_t reset)
+{
+    SPI.begin();
+    mfrc522.PCD_Init(cs, reset);
+    uint8_t v = mfrc522.PCD_ReadRegister(mfrc522.VersionReg);
+    if ((v == 0x00) || (v == 0xFF))
+        mfrc522.PCD_Init(cs, reset);
+    keyCS = cs;
+    keyReset = reset;
+}
+
+void rfidKey::loop(void)
+{
+    byte bufferATQA[2];
+    byte bufferSize = sizeof(bufferATQA);
+    MFRC522::StatusCode status = mfrc522.PICC_RequestA(bufferATQA, &bufferSize);
+    if (status != 0)
+        mfrc522.PCD_Init(keyCS, keyReset);
+    bool newCard = mfrc522.PICC_IsNewCardPresent();
+    if (newCard)
+    {
+        if (mfrc522.PICC_ReadCardSerial())
+        {
+            for (int i = 0; i < mfrc522.uid.size; i++)
+            {
+                UID.keyUID[i] = mfrc522.uid.uidByte[i];
+            }
+            UID.keySize = mfrc522.uid.size;
+            uint8_t buffer[UID.keySize];
+            memcpy(&buffer, &UID.keyUID, sizeof(UID.keyUID));
+            keyCRC = calcCRC16((uint8_t *)buffer, sizeof(buffer));
+        }
+        mfrc522.PICC_HaltA();
+        if (UID != lastUID)
+        {
+            if (onNewKeyFuncPtr != NULL)
+                onNewKeyFuncPtr(UID);
+        }
+        lastUID = UID;
+
+        if (onKeyFuncPtr != NULL)
+            onKeyFuncPtr(UID);
+    }
+}
+
+void rfidKey::getUID(UID_t &uid)
+{
+    uid = UID;
+}
+
+uint16_t rfidKey::getCRC(void)
+{
+    return keyCRC;
+}
+
+uint8_t rfidKey::getKeySize(void)
+{
+    return UID.keySize;
+}
+
+void rfidKey::printUID()
+{
+    for (int i = 0; i < UID.keySize; i++)
+    {
+        Serial.printf("%02X ", UID.keyUID[i]);
+    }
+    Serial.println();
+}
+
+void rfidKey::onKey(void (*FunPtr)(UID_t &uid))
+{
+    onKeyFuncPtr = FunPtr;
+}
+
+void rfidKey::onNewKey(void (*FunPtr)(UID_t &uid))
+{
+    onNewKeyFuncPtr = FunPtr;
+}
+
+void printUID(UID_t &uid)
+{
+    for (int i = 0; i < uid.keySize; i++)
+    {
+        Serial.printf("%02X ", uid.keyUID[i]);
+    }
+    Serial.println();
+}
+
+bool operator!=(UID_t &uid1, UID_t &uid2)
+{
+    bool result = false;
+    if (uid1.keySize != uid2.keySize)
+        result = true;
+    else
+    {
+        for (int i = 0; i < uid1.keySize; i++)
+        {
+            if (uid1.keyUID[i] != uid2.keyUID[i])
+                result = true;
+        }
+    }
+    return result;
+}
+
+bool operator==(UID_t &uid1, UID_t &uid2)
+{
+    return !(uid1 != uid2);
+}
---
+++ RfidKey/RfidKey.h
@@ -0,0 +1,70 @@
+/**************************************************************
+ * RfidKey
+ * (c) 2024 Juan M. Uribe
+ *
+ * This library allows easy use of the MFRC522 RFID card readers
+ ***************************************************************/
+#pragma once
+#include <SPI.h>
+#include <MFRC522.h>
+#include <CRC16.h>
+#include <CRC.h>
+
+#define REG_COUNT 10 // Maximum size of key
+
+typedef struct
+{
+    uint8_t keyUID[REG_COUNT];
+    uint8_t keySize;
+} UID_t;
+
+class rfidKey
+{
+private:
+    UID_t UID;
+    UID_t lastUID;
+    uint16_t keyCRC;
+    uint16_t keyCS, keyReset;
+    MFRC522 mfrc522;
+    void (*onKeyFuncPtr)(UID_t &UID) = NULL;
+    void (*onNewKeyFuncPtr)(UID_t &UID) = NULL;
+
+public:
+    // Constructor
+    rfidKey(uint16_t cs, uint16_t reset);
+
+    // Process de key. Must to be include in the loop
+    void loop(void);
+
+    // Returns the actual last readed UID
+    void getUID(UID_t &uid);
+
+    // Returns a 16 bits CRC of the last readed UID
+    uint16_t getCRC(void);
+
+    // Returns de size of the last readed UID
+    uint8_t getKeySize(void);
+
+    // Prints the last readed UID to Serial
+    void printUID(void);
+
+    // Sets a callback function called once a key is readed
+    // and returns the readed UID
+    void onKey(void (*FunPtr)(UID_t &uid));
+
+    // Sets a callback function called once the readed key is different
+    // to the last readed key and returns the readed UID
+    void onNewKey(void (*FunPtr)(UID_t &uid));
+};
+
+/***********************************************
+ * AUXILIARY FUNCTIONS
+ **********************************************/
+// operator == comparing UIDs
+bool operator==(UID_t &uid1, UID_t &uid2);
+
+// operator != comparing UIDs
+bool operator!=(UID_t &uid1, UID_t &uid2);
+
+// Prints an UID to Serial
+void printUID(UID_t &uid);
